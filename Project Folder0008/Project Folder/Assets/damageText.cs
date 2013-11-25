7b645ed600f8a0955cd50f0590a5e8bec0868dc9
Kitsune <lachlangodthelp@hotmail.com>
2013-11-25 12:51:49 +1100

    rawr3

---
+++ Project Folder0008/Project Folder/Assets/damageText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class damageText : MonoBehaviour {
+	float delay = 3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(this.transform.position.x,this.transform.position.y+0.05f,this.transform.position.z);
+		delay = delay-Time.deltaTime;
+		if(delay<=0){
+			DestroyImmediate(this.gameObject);
+		}
+	}
+}
