2531a78a354d9e5ba2650de6227a05934a1c9478
Mitchell Jenkins <mitchell_360@hotmail.com>
2013-12-06 11:03:44 +1100

    Regex the textfields. Attach to the labels not the textfields themselves

---
+++ Project Folder0008/Project Folder/Assets/C# Scripts/MenuScripts/TextFieldRegex.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+public class TextFieldRegex : MonoBehaviour {
+
+	UILabel uiLabelObject;
+	
+	void Start()
+	{
+		uiLabelObject = this.GetComponent<UILabel>();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+		uiLabelObject.text = Regex.Replace(uiLabelObject.text,"[^a-zA-Z0-9 ]","");
+	}
+}
